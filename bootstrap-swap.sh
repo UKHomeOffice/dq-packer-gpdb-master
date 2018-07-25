aab32282651619e1a517609f2d0d3e7dcbb30265
david-wirelab <info@wirelab.co.uk>
2018-07-25 15:44:29 +0100

    fix startup script

--- bootstrap-swap.sh
+++ bootstrap-swap.sh
@@ -4,7 +4,7 @@ export PATH=/sbin:/bin:/usr/sbin:/usr/bin
 set -e
 set -x
 
-if ! $(df -T | grep /dev/nvme0n1); then
+if ! $(df -T | grep -q /dev/nvme0n1); then
   mkfs.xfs -f /dev/nvme0n1
   mount -o nodev,noatime,inode64,allocsize=16m /dev/nvme0n1 /gpdb
   dd if=/dev/zero of=/dm-2 count=32768 bs=1MiB

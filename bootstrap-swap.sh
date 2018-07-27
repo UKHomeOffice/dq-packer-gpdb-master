b5ea9d04cb4e1c0530d95c178bb747a9bfbc3c10
david-wirelab <info@wirelab.co.uk>
2018-07-27 15:51:40 +0100

    change bootstrap and order of encrypted copies

--- bootstrap-swap.sh
+++ bootstrap-swap.sh
@@ -7,16 +7,13 @@ set -x
 if ! $(df -T | grep -q /dev/nvme0n1); then
   mkfs.xfs -f /dev/nvme0n1
   mount -o nodev,noatime,inode64,allocsize=16m /dev/nvme0n1 /gpdb
+  echo "/dev/nvme0n1 /gpdb xfs nodev,noatime,inode64,allocsize=16m 0 0" >> /etc/fstab
   dd if=/dev/zero of=/dm-2 count=32768 bs=1MiB
   chmod 600 /dm-2
   mkswap /dm-2
   swapon -v /dm-2
+  echo "/dm-2 swap swap defaults 0 0" >> /etc/fstab
   dhclient
 else
-  mount -o nodev,noatime,inode64,allocsize=16m /dev/nvme0n1 /gpdb
-  dd if=/dev/zero of=/dm-2 count=32768 bs=1MiB
-  chmod 600 /dm-2
-  mkswap /dm-2
-  swapon -v /dm-2
   dhclient
 fi
